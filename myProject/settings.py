e3ef8bc267303cc1bf541ed6faa2827b4639a450
renuka-33 <renukamuthuraman003@gmail.com>
2026-05-14 00:23:49 +0530

    ready for render deployment

--- myProject/settings.py
+++ myProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-f$frkm=2y83e@61jhh@1_*d&nh_$326(-4a8awv!nfhn*w=jxa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
 
 
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,7 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL =  '/images/'
 
 STATICFILES_DIRS = [
